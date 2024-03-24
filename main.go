0d4ea5ae769cb904bd39cb8cfb003b6e05841e10
jpcadinelli <jpcadinelli@gmail.com>
2024-03-24 19:33:21 -0300

    Curso Finalizado

--- main.go
+++ main.go
@@ -5,8 +5,17 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
-	contaExemplo := contas.ContaPoupanca{}
+	contaExemplo := contas.ContaCorrente{}
 	contaExemplo.Depositar(100)
+	PagarBoleto(&contaExemplo, 160)
 	fmt.Println(contaExemplo.ObterSaldo())
 }
